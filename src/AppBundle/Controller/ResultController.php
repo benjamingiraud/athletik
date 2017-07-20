7b6f83bc937ac7a1805f088a0d906a720fae89b8
benjamingiraud <benji29410@outlook.com>
2017-07-20 16:48:14 +0200

    affichage resultat de l'année

--- src/AppBundle/Controller/ResultController.php
+++ src/AppBundle/Controller/ResultController.php
@@ -16,6 +16,22 @@ class ResultController extends Controller
     public function showAction()
     {
         $em = $this->getDoctrine()->getManager();
+        $sql = "SELECT SUM(result.points) as total, SUM(result.time) as ttime, athlete.lastname, athlete.firstname, category.name "
+                . "FROM result "
+                . "inner join athlete on result.athlete_id = athlete.id "
+                . "inner join meeting on result.meeting_id = meeting.id "
+                . "inner join category on athlete.category = category.id "
+                . "WHERE YEAR(CURRENT_DATE()) = 2017 "
+                . "GROUP BY athlete.id "
+                . "ORDER BY total DESC ";
+        $nativeQuery = $em->getConnection()->prepare($sql);
+        $nativeQuery->execute();
+        $general_classment = $nativeQuery->fetchAll();
+//        $query_general  = $em->createQuery('SELECT SUM(r.points) as total
+//                                            FROM AppBundle:Result r,
+//                                            GROUP BY r.ahlete
+//                                            ORDER BY total DESC');
+//        $general_classment = $query_general->getResult();
         
         $query_meetings = $em->createQuery('SELECT m
                                             FROM AppBundle:Meeting m
@@ -28,7 +44,8 @@ class ResultController extends Controller
         
         return $this->render('results.html.twig', 
               array ('finished_meetings' => $finished_meetings,
-                     'results_meetings'  => $results_meetings
+                     'results_meetings'  => $results_meetings,
+                     'generals'          => $general_classment
                     ));
 //                       
 //        $query = $em->createQuery('SELECT COUNT(r.meeting) as nb, IDENTITY(r.meeting) as id
